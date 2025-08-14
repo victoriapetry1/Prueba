c617b15aec43c2d647cd88707ad370837a5e3621
Lautaro <150679167+LautaroRoldan865@users.noreply.github.com>
2025-08-14 19:49:37 -0300

    home

--- front-prueba/src/app/app.routes.ts
+++ front-prueba/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: Home },
+];
